bc1fd5a4c243d19a3936c4f913f587ef8df252af
Muhammad Shahid Raza <Muhammadshahidraza8@gmail.com>
2023-03-16 00:36:36 +0500

    response codes

---
+++ Obaki/src/constants/responseCodes.js
@@ -0,0 +1,13 @@
+export const LOGIN_SUCCESSFUL = "Login successfull!";
+export const REGISTRATION_SUCCESSFUL = "Registration successfull!";
+export const FORGOT_PASSWORD_SUCCESSFUL = "Email sent!";
+export const ADDRESS_ADDED = "Address added!";
+export const UPDATE_SUCCESSFUL = "Update successful!";
+export const PASSWORD_CHANGED_SUCCESSFUL = "Password changed successfully";
+export const AUTHENTICATED = "Authenticated!";
+export const ADDRESS_DELETED = "Address deleted";
+export const ADDED_TO_CART = "Product added to cart";
+export const REMOVED_FROM_WISHLIST = "Product removed from wishlist";
+export const ADDED_TO_WISHLIST = "Product added to wishlist";
+export const SENT_VERIFICATION_CODE = "We have sent a 6 digit code to email !";
+
