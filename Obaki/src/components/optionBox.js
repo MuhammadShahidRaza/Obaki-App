83782d6d8b34a818e9cbafcc324a1f4934a9f7d2
Muhammad Shahid Raza <Muhammadshahidraza8@gmail.com>
2023-03-19 21:36:52 +0500

    option box

---
+++ Obaki/src/components/optionBox.js
@@ -0,0 +1,43 @@
+import {Dimensions, View, Text ,TouchableOpacity,Image} from 'react-native'
+import React from 'react'
+import { Color } from '../utils/color'
+
+const {width} = Dimensions.get('window');
+
+const OptionBox = ({image,title,onPress}) => {
+  return (
+    <TouchableOpacity onPress={onPress}>
+    <View
+      style={{
+        borderWidth: 1,
+        borderRadius: 20,
+        borderColor: Color.BLACK,
+        width: width - 100,
+        paddingHorizontal: 20,
+        paddingVertical: 20,
+        marginHorizontal: 20,
+        marginTop: 20,
+        backgroundColor: Color.WHITE,
+        display: 'flex',
+        alignItems: 'center',
+      }}>
+      <Text
+        style={{
+          fontSize: 20,
+          fontWeight: 'bold',
+          marginBottom: 20,
+          textAlign: 'center',
+        }}>
+       {title}
+      </Text>
+      <Image
+        source={image}
+        resizeMode={"contain"}
+        style={{width: 120, height: 100}}
+      />
+    </View>
+    </TouchableOpacity>
+  )
+}
+
+export default OptionBox
